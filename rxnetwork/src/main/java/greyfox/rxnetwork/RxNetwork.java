61f3cae540489ae22007b557f0edcf6ff00a5c34
Radek Kozak <radoslaw.kozak@gmail.com>
2017-05-20 13:52:31 +0200

    Clean up for lint

--- rxnetwork/src/main/java/greyfox/rxnetwork/RxNetwork.java
+++ rxnetwork/src/main/java/greyfox/rxnetwork/RxNetwork.java
@@ -41,9 +41,9 @@ import java.util.Collection;
 
 /**
  * RxNetwork is a class that listens to network connectivity changes in a reactive manner.
- * It uses default {@link BuiltInNetworkObservingStrategyFactory factory} under the hood to along
- * with provider mechanism to choose concrete, api-level dependent {@link NetworkObservingStrategy strategy}
- * for observing network connectivity changes.
+ * It uses default {@link BuiltInNetworkObservingStrategyFactory factory} under the hood along with
+ * provider mechanism to choose concrete, api-level dependent
+ * {@link NetworkObservingStrategy strategy} for observing network connectivity changes.
  * <p>
  * Custom factory can be used by simply implementing {@link NetworkObservingStrategyFactory}
  * and registering it with {@linkplain RxNetwork}:
